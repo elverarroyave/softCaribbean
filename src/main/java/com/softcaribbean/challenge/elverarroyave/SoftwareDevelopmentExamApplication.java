a63fcdb7edb9ff038cb3f685f0432f839cdb880a
elverarroyave01 <arroyave.elver@gmail.com>
2021-09-18 11:23:10 -0500

    git init project

---
+++ src/main/java/com/softcaribbean/challenge/elverarroyave/SoftwareDevelopmentExamApplication.java
@@ -0,0 +1,13 @@
+package com.softcaribbean.challenge.elverarroyave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareDevelopmentExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareDevelopmentExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softcaribbean/challenge/elverarroyave/SoftwareDevelopmentExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softcaribbean.challenge.elverarroyave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareDevelopmentExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
